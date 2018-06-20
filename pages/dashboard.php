f0d53cd64045457cc17677190d15e747171c578f
M Umar Ramadhana <umarkotak@yahoo.co.id>
2018-06-20 15:20:36 +0700

    admin dashboard works

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,3 +1,19 @@
+<?php
+$sql = $conn->prepare("SELECT * FROM certificates WHERE status = 'verified'");
+$data = array();
+$sql->execute($data);
+$verified_count = $sql->rowcount();
+$sql = $conn->prepare("SELECT * FROM certificates");
+$sql->execute($data);
+$uploaded_count = $sql->rowcount();
+$sql = $conn->prepare("SELECT * FROM certificates WHERE status = 'fake'");
+$sql->execute($data);
+$fake_count = $sql->rowcount();
+$sql = $conn->prepare("SELECT * FROM published_certificates");
+$sql->execute($data);
+$published_count = $sql->rowcount();
+?>
+
 <!-- Content Header (Page header) -->
 <section class="content-header">
   <h1>
@@ -17,7 +33,7 @@
     <div class="col-lg-3 col-xs-6">
       <div class="small-box bg-aqua">
         <div class="inner">
-          <h3>150</h3>
+          <h3><?php echo $published_count; ?></h3>
 
           <p>Sertifikat Terpublish</p>
         </div>
@@ -31,7 +47,7 @@
     <div class="col-lg-3 col-xs-6">
       <div class="small-box bg-yellow">
         <div class="inner">
-          <h3>44</h3>
+          <h3><?php echo $uploaded_count; ?></h3>
 
           <p>Sertifikat Terupload</p>
         </div>
@@ -45,7 +61,7 @@
     <div class="col-lg-3 col-xs-6">
       <div class="small-box bg-green">
         <div class="inner">
-          <h3>53<sup style="font-size: 20px">%</sup></h3>
+          <h3><?php echo $verified_count; ?></h3>
 
           <p>Sertifikat Asli</p>
         </div>
@@ -59,7 +75,7 @@
     <div class="col-lg-3 col-xs-6">
       <div class="small-box bg-red">
         <div class="inner">
-          <h3>65</h3>
+          <h3><?php echo $fake_count; ?></h3>
 
           <p>Sertifikat Palsu</p>
         </div>
